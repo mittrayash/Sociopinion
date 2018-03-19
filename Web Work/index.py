e980077cd7af6023543324b6eb0afaa8ec7eb533
Vasu7052 <dev.vasugupta@gmail.com>
2018-03-19 20:17:41 +0530

    Created Front Page

--- Web Work/index.py
+++ Web Work/index.py
@@ -1,7 +1,17 @@
-from flask import Flask
-import numpy as np
+from flask import Flask,render_template
 
+app = Flask(__name__)
 
+@app.route("/")
+def hello():
+    return render_template('index.html')
 
+@app.route("/main")
+def main_page():
 
+    
 
+    return render_template('main.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
