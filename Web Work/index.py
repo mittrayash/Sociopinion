996ee2dd7ed2b5653f8a8ceec9e54b9cc94a953d
Vasu7052 <dev.vasugupta@gmail.com>
2018-03-20 17:20:42 +0530

    Finally Completed all 3 graphs with changed code and showing legends.

--- Web Work/index.py
+++ Web Work/index.py
@@ -10,6 +10,7 @@ import nltk
 from tweeter2 import search
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
 
 def getText():
     with open('static/dynamic.txt') as f:
@@ -38,27 +39,59 @@ def main_page():
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth, wait_on_rate_limit=True)
 
-    nltk.download('vader_lexicon')
+    #nltk.download('vader_lexicon')
 
     for i in range(10):
         N = 10 - i
         date_start = datetime.now() - timedelta(days=N)
         date_until = datetime.now() - timedelta(days=N - 2)
 
-        lis, counts = search(api, 'modi', 100, date_start, date_until)
+        lis, counts = search(api, query, 23, date_start, date_until)
         print(counts)
 
         final_list.append(lis)
         final_count.append(counts)
 
+    for i in range(len(final_count)):
+        temp_list = [final_count[i]['Positive'] , final_count[i]['Neutral'] , final_count[i]['Negative']]
+        final_count[i] = temp_list
 
+    pos_line_graph_list = []
+    neu_line_graph_list = []
+    neg_line_graph_list = []
 
-    print(lis, counts)
-    data = {'tweets': lis}
+    for i in range(len(final_count)):
+        pos_line_graph_list.append(final_count[i][0])
+        neu_line_graph_list.append(final_count[i][1])
+        neg_line_graph_list.append(final_count[i][2])
+
+    total_pos = 0
+    total_neu = 0
+    total_neg = 0
+    avg_pos = 0
+    avg_neu = 0
+    avg_neg = 0
+
+    for i in range(len(final_count)):
+        total_pos += final_count[i][0]
+        total_neu += final_count[i][1]
+        total_neg += final_count[i][2]
+
+    avg_pos = total_pos/(total_pos + total_neg)
+    avg_neg = total_neg/(total_pos + total_neg)
+
+    data = {'tweets': final_list , 'pos_line_data' : pos_line_graph_list
+            , 'neu_line_data' : neu_line_graph_list , 'neg_line_data' : neg_line_graph_list
+            , 'total_pos' : total_pos , 'total_neu' : total_neu
+            ,'total_neg' : total_neg ,'avg_pos' : avg_pos
+            , 'avg_neg' : avg_neg}
+
+    print(data)
+
     return render_template('main.html', data=data)
 
 @app.route('/compare')
-def comapre_page():
+def compare_page():
     return render_template('compare.html')
 
 if __name__ == '__main__':
