b0c6fe12b46651dcfd655fe3d3c65f95d72c53f4
Vasu7052 <dev.vasugupta@gmail.com>
2018-03-21 02:56:03 +0530

    Some Changes in Graph

--- Web Work/index.py
+++ Web Work/index.py
@@ -26,7 +26,7 @@ def main_page():
     final_list = []
     final_count = []
 
-    consumer_key = 'AYDNqr9ycBI9qaOWoYXJgYnKY'     
+    consumer_key = 'AYDNqr9ycBI9qaOWoYXJgYnKY'
     consumer_secret = 'tSHVPOr6JrQ9KNnqX1aSOGnKecmPeQQ37j81JAURI0t6deb8AA'
     access_token = '2493864625-SSCalZj2of8hITNgrv4gMvNZX7seGTSWD2MQI0H'
     access_token_secret = '5JBwqg2jWjijXgIRpEsdLrGRYWEuNmc4M0ibfRL2xzrhd'
